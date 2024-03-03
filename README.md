dd73d0f8b711ee7d9976570dbd94f1692e415e49
Vikkeyeftimov <Vikksaus@hotmail.com>
2024-03-03 20:13:14 +0100

    laatste werk

--- README.md
+++ README.md
@@ -1,32 +1,15 @@
-> _Fork_ deze leertaak en ga aan de slag. Onderstaande outline ga je gedurende deze taak in jouw eigen GitHub omgeving uitwerken. De instructie vind je in: [docs/INSTRUCTIONS.md](docs/INSTRUCTIONS.md)
+# brilletjes-squad-page
 
-# Titel
-<!-- Geef je project een titel en schrijf in één zin wat het is -->
+![Screenshot 2024-02-28 211441](https://github.com/christoph3r3w/brilletjes-squad-page/assets/144007933/a7f66f66-7fbe-45a9-9eec-d6cd9a664a5e)
 
-## Inhoudsopgave
+# team leden
+[Anna-Kyra](https://github.com/Anna-Kyra)
 
-  * [Beschrijving](#beschrijving)
-  * [Kenmerken](#kenmerken)
-  * [Installatie](#installatie)
-  * [Gebruik](#gebruik)
-  * [Bronnen](#bronnen)
-  * [Licentie](#licentie)
+[Viktor](https://github.com/Vikkeyeftimov)
 
-## Beschrijving
-<!-- In de Beschrijving staat hoe je project er uit ziet, hoe het werkt en wat je er mee kan. -->
-<!-- Voeg een mooie poster visual toe 📸 -->
-<!-- Voeg een link toe naar Github Pages 🌐-->
+[Christopher](https://github.com/christoph3r3w)
 
-## Kenmerken
-<!-- Bij Kenmerken staat welke technieken zijn gebruikt en hoe. Wat is de HTML structuur? Wat zijn de belangrijkste dingen in CSS? Wat is er met Javascript gedaan en hoe? Misschien heb je een framwork of library gebruikt? -->
+# team links 
+[miro](https://miro.com/welcomeonboard/dWh5UTJmeFh6MmR0b21HdGhwMEtuajRjOW1NVUlvR093MlNheEhZNzRxdFNSWTFITTNlTmpKZ29nd2VnaWNueHwzNDU4NzY0NTYzNTk1MTIwNzE5fDI=?share_link_id=71326057584)
 
-## Installatie
-<!-- Bij Installatie staat stap-voor-stap beschreven hoe je de development omgeving moet inrichten om aan de repository te kunnen werken. -->
-
-## Gebruik
-
-## Bronnen
-
-## Licentie
-
-This project is licensed under the terms of the [MIT license](./LICENSE).
+# kenmerken 
--- server.js
+++ server.js
@@ -7,11 +7,19 @@ import fetchJson from './helpers/fetch-json.js'
 // Stel het basis endpoint in
 const apiUrl = 'https://fdnd.directus.app/items'
 
+const data_c = await fetchJson('https://fdnd.directus.app/items/person/40')
+const data_k = await fetchJson('https://fdnd.directus.app/items/person/41')
+const data_v = await fetchJson('https://fdnd.directus.app/items/person/15')
+console.log(data_k.data.id)
+
 // Haal alle squads uit de WHOIS API op
 const squadData = await fetchJson(apiUrl + '/squad')
-
+// console.log(squadData.data)
 // Maak een nieuwe express app aan
 const app = express()
+
+const game_element = [];
+// console.log(game_element)
 
 // Stel ejs in als template engine
 app.set('view engine', 'ejs')
@@ -22,23 +30,83 @@ app.set('views', './views')
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
 
+app.use(express.urlencoded({extented:true}))
+
 // Maak een GET route voor de index
 app.get('/', function (request, response) {
   // Haal alle personen uit de WHOIS API op
   fetchJson(apiUrl + '/person').then((apiData) => {
     // apiData bevat gegevens van alle personen uit alle squads
     // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
-
+    
     // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
-    response.render('index', {persons: apiData.data, squads: squadData.data})
+    response.render('index', {
+      persons: apiData.data, 
+      squads: squadData.data, 
+      data_c: data_c.data,
+      data_v: data_v.data,
+      data_k: data_k.data
+    })
+    // response.render('index', data)
+
   })
 })
 
-// Maak een POST route voor de index
+
+
+// Maak een POST route voor de window squad
+// console.log("yes1")
 app.post('/', function (request, response) {
   // Er is nog geen afhandeling van POST, redirect naar GET op /
-  response.redirect(303, '/')
-})
+
+  console.log(request.body)
+  
+  // console.log(apiResponse)
+  // fetchJson('https://fdnd.directus.app/items/person/40' ).then((apiResponse) => {
+
+  // try {
+  //   apiData.data.custom = JSON.parse(apiData.data.custom)
+  // } catch (error) {
+  //   apiResponse.data.custom = []
+  // }
+
+
+  // if (!apiResponse.data.custom.game_element) {
+  //   apiResponse.data.custom.game_element = []
+  // } else if(request.body.element == "fire"){
+  //   console.log("fire")
+  // } else if(request.body.element == "water") {
+  //   console.log("water")
+  // } else if(request.body.element == "earth") {
+  //   console.log("earth")
+  // } else if(request.body.element == "wind") {
+  //   console.log("wind")
+  // }else{
+  //   console.log("default")
+  // };
+
+  // console.log("POST request successful");
+ 
+
+  // fetch('https://fdnd.directus.app/items/person/' + request.params.id, {
+  //     method: 'PATCH',
+  //     body: JSON.stringify({
+  //       custom: apiResponse.data.custom
+  //     }),
+  //     headers: {
+  //       'Content-type': 'application/json; charset=UTF-8'
+  //     }
+  //   }).then((patchResponse) => {
+  //     // Redirect naar de persoon pagina
+  //     response.redirect(303, '/detail/' + request.params.id)
+  //   })
+  // })
+  
+
+  response.redirect(303, '/');
+});
+
+
 
 // Maak een GET route voor een detailpagina met een request parameter id
 app.get('/person/:id', function (request, response) {
@@ -50,7 +118,7 @@ app.get('/person/:id', function (request, response) {
 })
 
 // Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8000)
+app.set('port', process.env.PORT || 8002)
 
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
